151d0ba6433c744cdd2d9ef69b261579e383245e
unknown <kovgankosvetlana.com>
2022-02-17 00:38:59 +0300

    1.3

--- write_phone.py
+++ write_phone.py
@@ -4,8 +4,8 @@ def write_line(name, surname, phone, comment):
     global data
     file1 = data
     with open(file1, 'a') as f:
-        f.write(f'{name};{surname};{phone};{comment}\n')
-        f.write('')
+        f.write(f'{name};{surname};{phone};{comment}')
+        #f.write('')
     return file1
 
 
@@ -13,6 +13,6 @@ def write_column(name, surname, phone, comment):
     global data
     file2 = data
     with open(file2, 'a') as f:
-        f.write(f'{name};\n{surname};\n{phone};\n{comment};\n')
-        f.write('')
+        f.write(f'{name};\n{surname};\n{phone};\n{comment};')
+        #f.write('')
     return file2
